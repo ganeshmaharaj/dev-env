a2b4a4a801bdf905bb5b88023b51ffc7cb7a913b
Ganesh Mahalingam <ganesh.mahalingam@intel.com>
2017-11-06 20:40:06 -0800

    Fixes for badly written script in haste
    Signed-off-by: Ganesh Mahalingam <ganesh.mahalingam@intel.com>

--- kube/install-kube.sh
+++ kube/install-kube.sh
@@ -5,28 +5,31 @@
 
 # Peg the versions to the ones below as that is what openstack-helm uses. We can
 # move it to newer ones when we stop using them or stop working on openstack-helm
-KUBE_VERSION=v1.7.5
+# Using kube version from the apt sources against downloading the tar like OSH does.
+KUBE_VERSION="1.7.5-00"
 HELM_VERSION=v2.6.1
 TMP_DIR=$(mktemp -d)
 
 echo "Installing Kubernetes..."
 sudo -E bash -c "cat << EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt/kubernetes.io/ kubernetes-xenial-unstable main
+deb http://apt.kubernetes.io/ kubernetes-xenial-unstable main
 EOF"
 
 echo "Get keys..."
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo -E apt-key add -
 
 sudo -E apt update
-sudo -E install -y docker.io kubelet="${KUBE_VERSION}" kubeadm="${KUBE_VERSION}" \
-          kubectl="${KUBE_VERSION}"
+sudo -E apt install -y docker.io kubelet="${KUBE_VERSION}" kubeadm="${KUBE_VERSION}" \
+            kubectl="${KUBE_VERSION}"
 sudo -E apt-mark hold kubelet kubeadm kubectl
 
+# Setting the subnet static here for now.
 echo "Setup kube on this system"
 sudo -E kubeadm init --pod-network-cidr 10.244.0.0/16
 
 echo "Now let us install helm.."
-curl -sSL https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VERSION}-linux-amd64.tar.gz | tar -zxv --strip-components=1 -C ${TMP_DIR}sudo mv ${TMP_DIR}/helm /usr/local/bin/helm
+curl -sSL https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VERSION}-linux-amd64.tar.gz | tar -zxv --strip-components=1 -C ${TMP_DIR}
+sudo mv ${TMP_DIR}/helm /usr/local/bin/helm
 rm -rf ${TMP_DIR}
 
 echo "Setting some env variables.. Writing them to your bashrc.."
