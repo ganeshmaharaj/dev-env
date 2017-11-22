6a1c69de2cef94b9cfce795a9d198c49376a9a36
Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>
2017-11-22 09:15:06 -0800

    updates to kube script to handle dnf systems
    Signed-off-by: Ganesh Maharaj Mahalingam <ganesh.mahalingam@intel.com>

--- kube/install-kube.sh
+++ kube/install-kube.sh
@@ -11,19 +11,51 @@ HELM_VERSION=v2.6.1
 TMP_DIR=$(mktemp -d)
 SCRIPT_ENV="KUBECONFIG=/etc/kubernetes/admin.conf "
 
-echo "Installing Kubernetes..."
-sudo -E bash -c "cat << EOF > /etc/apt/sources.list.d/kubernetes.list
+function apt_install() {
+  echo "Installing Kubernetes in an apt based system..."
+  sudo -E bash -c "cat << EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF"
 
-echo "Get keys..."
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo -E apt-key add -
+  echo "Get keys..."
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo -E apt-key add -
 
-sudo -E apt update
-sudo -E apt install -y make git curl
-sudo -E apt install -y docker.io kubelet="${KUBE_VERSION}" kubeadm="${KUBE_VERSION}" \
-            kubectl="${KUBE_VERSION}"
-sudo -E apt-mark hold kubelet kubeadm kubectl
+  sudo -E apt update
+  sudo -E apt install -y make git curl
+  sudo -E apt install -y docker.io kubelet="${KUBE_VERSION}" kubeadm="${KUBE_VERSION}" \
+              kubectl="${KUBE_VERSION}"
+  sudo -E apt-mark hold kubelet kubeadm kubectl
+}
+
+function dnf_install() {
+  echo "Installing Kubernetes in a dnf based system..."
+  sudo -E bash -c "cat << EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF"
+
+  echo "put selinux setenforce to 0 until kubelet can handle selinux better..."
+  sudo -E setenforce 0
+  sudo dnf -y update
+  sudo dnf install -y  make git curl wget
+  sudo dnf install -y kubelet-${KUBE_VERSION} kubeadm-${KUBE_VERSION} kubectl-${KUBE_VERSION} docker
+  sudo systemctl enable kubelet && sudo systemctl start kubelet
+  sudo systemctl enable docker && sudo systemctl start docker
+}
+
+if [ -f /etc/redhat-release ]; then
+  dnf_install
+elif [ -f /etc/lsb-release ]; then
+  apt_install
+else
+  echo "Unable to find the OS type to use a package manager."
+  exit 1
+fi
 
 # Setting the subnet static here for now.
 # Running kubeadm reset cause of issue https://github.com/kubernetes/kubernetes/issues/53356
@@ -47,7 +79,7 @@ echo "Now let us install helm.."
 curl -sSL https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VERSION}-linux-amd64.tar.gz | tar -zxv --strip-components=1 -C ${TMP_DIR}
 sudo mv ${TMP_DIR}/helm /usr/local/bin/helm
 rm -rf ${TMP_DIR}
-sudo -E ${SCRIPT_ENV} helm init
+sudo -E ${SCRIPT_ENV} /usr/local/bin/helm init
 
 echo "Setting some env variables.. Writing them to your bashrc.."
 echo "export KUBE_VERSION=${KUBE_VERSION}" >> ~/.bashrc
